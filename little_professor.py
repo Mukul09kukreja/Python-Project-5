85750106e9896f7d93190ce178b0cc079da9e0bf
mukul <mukulkukreja009@gmail.com>
2026-03-07 00:38:08 +0530

    develop a maths solving game of two level

---
+++ little_professor.py
@@ -0,0 +1,63 @@
+import random
+
+def main():
+  get_level() 
+  
+def get_level():
+    while True:
+      try:
+        levelp = int(input("Choose Level 1 or 2: "))
+        break
+      except ValueError:
+          continue
+      except KeyboardInterrupt:
+        print("If you want exit to it type exit")
+    
+    generate_integer(levelp)
+
+def generate_integer(b):
+  if b == 1:
+    sum = 0
+    for i in range(0,17):
+      try:
+        x = random.choice([1,2,3,4,5,6,7,8,9])
+        y = random.choice([1,2,3,4,5,6,7,8,9])
+        inputp= int(input(f"{x} + {y} = "))
+        if x+y == inputp:
+          sum += i
+        else:
+           print("EEE")
+           continue
+      except ValueError:
+          print("EEE")
+          continue
+      
+      print(f"Score is {sum}")
+  elif b == 2:
+    sum = 0
+    for i in range(0,17):
+      try:
+        x = random.choice([1,2,3,4,5,6,7,8,9])
+        y = random.choice([1,2,3,4,5,6,7,8,9])
+        t = random.choice(["+", "-"])
+        inputp= int(input(f"{x} {t} {y} = "))
+        if x+y == inputp:
+          sum += i
+        elif x-y == inputp:
+           sum += i
+        elif (x+y or x-y) == "exit":
+           break
+        else:
+           print("EEE")
+           continue
+      except ValueError:
+          print("EEE")
+          continue
+      except KeyboardInterrupt:
+         print("Are you want to exit type 'exit")
+      
+      
+    print(f"Score is {sum}")
+       
+if __name__ == "__main__":
+    main()
